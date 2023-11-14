1870e1b90c2c8bc4d6f4e7cb358d23918e3f33c2
vallotj <vallotj@softedit.fr>
2023-11-14 15:20:23 +0100

    Initial commit v1.0.0

---
+++ README.md
@@ -0,0 +1,9 @@
+# UPC Impression
+
+## Module qui permet d'imprimer 
+
+### INSTALLATION 
+
+1. unzip module_upcimpression_1.0.0.zip dans le dossier custom(recommandé) ou htdocs
+2. Activer le module
+3. Imprimez un ticket
---
+++ class/actions_upcimpression.class.php
@@ -0,0 +1,123 @@
+<?php
+/* Copyright (C) ---Put here your own copyright and developer email---
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file    htdocs/modulebuilder/template/class/actions_mymodule.class.php
+ * \ingroup mymodule
+ * \brief   Example hook overload.
+ *
+ * Put detailed description here.
+ */
+
+/**
+ * Class ActionsMyModule
+ */
+class ActionsUPCImpression
+{
+    /**
+     * @var DoliDB Database handler.
+     */
+    public $db;
+
+    /**
+     * @var string Error code (or message)
+     */
+    public $error = '';
+
+    /**
+     * @var array Errors
+     */
+    public $errors = array();
+
+
+    /**
+     * @var array Hook results. Propagated to $hookmanager->resArray for later reuse
+     */
+    public $results = array();
+
+    /**
+     * @var string String displayed by executeHook() immediately after return
+     */
+    public $resprints;
+
+    /**
+     * @var int		Priority of hook (50 is used if value is not defined)
+     */
+    public $priority;
+
+
+    /**
+     * Constructor
+     *
+     *  @param		DoliDB		$db      Database handler
+     */
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+
+    /**
+     * Execute action
+     *
+     * @param	array			$parameters		Array of parameters
+     * @param	CommonObject    $object         The object to process (an invoice if you are in invoice module, a propale in propale's module, etc...)
+     * @param	string			$action      	'add', 'update', 'view'
+     * @return	int         					<0 if KO,
+     *                           				=0 if OK but we want to process standard actions too,
+     *                            				>0 if OK and we want to replace standard actions.
+     */
+    public function getNomUrl($parameters, &$object, &$action)
+    {
+        global $db, $langs, $conf, $user;
+        $this->resprints = '';
+        return 0;
+    }
+
+    /**
+     * Overloading the doActions function : replacing the parent's function with the one below
+     *
+     * @param   array           $parameters     Hook metadatas (context, etc...)
+     * @param   CommonObject    $object         The object to process (an invoice if you are in invoice module, a propale in propale's module, etc...)
+     * @param   string          $action         Current action (if set). Generally create or edit or null
+     * @param   HookManager     $hookmanager    Hook manager propagated to allow calling another hook
+     * @return  int                             < 0 on error, 0 on success, 1 to replace standard code
+     */
+    public function doActions($parameters, &$object, &$action, $hookmanager)
+    {
+        global $conf, $user, $langs, $db;
+
+        if (in_array($parameters['currentcontext'], array('takeposinvoice'))) {
+?>
+            <script type="text/javascript">
+                $('#buttonprint').attr("onclick", "UPCTakePosPriceTTC(<?php echo $object->id ?>)");
+                function UPCTakePosPriceTTC(id){
+                    console.log("UPC DolibarrTakeposPrinting Printing invoice ticket " + id)
+                    $.ajax({
+                        type: "GET",
+                        data: { token: '<?php echo currentToken(); ?>' },
+                        url: "<?php print DOL_URL_ROOT."/custom/upcimpression/ajax/ajax.php?action=printinvoiceticket&token=".newToken()."&term=".urlencode(isset($_SESSION["takeposterminal"]) ? $_SESSION["takeposterminal"] : "ww")."&id="; ?>" + id,
+                    });
+                }
+            </script>
+<?php
+        }
+        return 0;
+    }
+
+
+}
